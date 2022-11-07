e132827a716b8a6661ad3a3447ebdbbb4fbded71
Abhishek Rana <abhishekrana21092003@gmail.com>
2022-11-07 11:32:02 +0530

    Initial Commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -64,14 +64,14 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
                 <h4>SQL</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className='text-light'>Basic</small>
               </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
                 <h4>C#</h4>
-                <small className='text-light'>Novice</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>

845cb5852f3abe504426093d9b7f2eaa66978083
Abhishek Rana <abhishekrana21092003@gmail.com>
2023-02-14 20:32:21 +0530

    Initial Commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import './portfolio.css'
 import IMG1 from '../../assets/img/portfolio1.png'
 import IMG2 from '../../assets/img/portfolio2.png'
 import IMG3 from '../../assets/img/portfolio3.png'
+import IMG4 from '../../assets/img/portfolio4.png'
 
 const Portfolio = () => {
   return (
@@ -13,12 +14,22 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
+            <img src={IMG4} alt='AR Cube: Augmented Reality App' />
+          </div>
+          <h3>AR Cube: Augmented Reality App</h3>
+          <div className="portfolio__item-cta">
+          <a href='https://play.google.com/store/apps/details?id=com.ZelloSpark.ARCube' className='btn btn-primary' target='_blank'>Play Store</a>
+          <a href='https://blogs.zellospark.com/2023/02/projects-arcube.html' className='btn' target='_blank'>Blog</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
             <img src={IMG3} alt='Drawlax: Fun Drawing Game' />
           </div>
           <h3>Drawlax: Fun Drawing Game</h3>
           <div className="portfolio__item-cta">
-            <a href='https://www.zellospark.com/2022/10/projects-drawlax.html' className='btn' target='_blank'>Blog</a>
-            <a href='https://play.google.com/store/apps/details?id=com.ZelloSpark.Drawlax' className='btn btn-primary' target='_blank'>Play Store</a>
+          <a href='https://play.google.com/store/apps/details?id=com.ZelloSpark.Drawlax' className='btn btn-primary' target='_blank'>Play Store</a>
+            <a href='https://blogs.zellospark.com/2022/10/projects-drawlax.html' className='btn' target='_blank'>Blog</a>
           </div>
         </article>
         <article className='portfolio__item'>
